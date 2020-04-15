9f275d856f9e6d0a7488fce639556c98d12918d4
Mikey Joyce <joyce.mp2019@gmail.com>
2020-04-15 16:34:33 -0500

    Readability
    Increased the readability of the code by adding lots of comments. Also decided to add a saftey check in enqueue to make sure createNode is returning the proper data. Showed in the comments that enqueue is O(1) complexity and dequeue is O(N) complexity. I could eventually make dequeue O(1) complexity by adding a struct that keeps the data for the tail of the queue.

--- queue.c
+++ queue.c
@@ -1,23 +1,43 @@
 #include "queue.h"
 
+/*This function creates and enqueues a List *node with an integer as data
+into the queue. Has O(1) complexity.*/
 List *enqueue(List *node, int data){
+	//Declare and initialize the new node with the given data
 	List *newNode;
 	newNode = createNode(data);
-	newNode->next=node;
 
-	return newNode;
+	//If valid
+	if(newNode){
+		//Set the new node to be the head of the list
+		newNode->next=node;
+
+		//Return the new head of the list
+		return newNode;
+	}
+	else{
+		//Node was not valid
+		return NULL;
+	}
 }
 
+/*This function takes a pointer to a list and deletes the tail of the list.
+Has O(N) complexity.*/
 List *dequeue(List *node){
+	//create temporary nodes
 	List *temp=node;
 	List *temp2;
 
+	//traverse the list
 	while(temp->next!=NULL){
+		//Obtains the tail of the list
 		temp2=temp;
 		temp=temp->next;
 	}
+	//Once node is found free memory and set its next pointer to NULL
 	free(temp2->next);
 	temp2->next=NULL;
 
+	//return head of the list.
 	return node;
 }
